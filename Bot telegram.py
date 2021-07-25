77e2a97c580104746dd48ee6df4f2661fb7f4e9f
BOOS <skaysnhsn329@gmail.com>
2021-07-25 13:49:08 +0300

    Add files via upload
    #Telegram bot code

---
+++ Bot telegram.py
@@ -0,0 +1,21 @@
+#/!/user/bin/env python3
+#the cooding from ABN BOOS
+# *******************************************************************
+# ***  BOT TELEGRAM ***
+# ** **** Version ***  :
+# ***   v0.1.   ***
+# ****** Date.  *****:
+#   12 - 40 - 2021 { SAT 24  July 2021 }
+#***********************************************************
+from telegram.ext import updater
+u = Updater('كود يتم أرساله من شركة تلغرام يسمى بالمعرف الخاص للبوت')
+j = u.job.queue
+from telegram.ext import Job
+def callback minute(bot,job)
+    bot.send_ messages(chat_id=@'tgrball')
+                           text=('الرسالة للأولى')
+Job_minute = Job(callback_ minute,5.0) 
+j.put(jop_minute)    
+              
+ 
+ 
